a576254e22502e677760c124c49ace4e348b3d74
Luís Fernando <luis.moraes@nddigital.com.br>
2014-07-10 17:40:41 -0300

    Adicionando codigo para mudança do lookAndFeel

--- JRestart/src/com/br/huehue/jrestart/Main.java
+++ JRestart/src/com/br/huehue/jrestart/Main.java
@@ -1,11 +1,26 @@
 package com.br.huehue.jrestart;
 
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println(":D");
+		try {
+		    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+		        if ("Nimbus".equals(info.getName())) {
+		            UIManager.setLookAndFeel(info.getClassName());
+		            break;
+		        }
+		        System.out.println(info.getName());
+		    }
+		} catch (Exception e) {
+		    System.err.print("Erro: "+e);
+		}
 		
-
+		JOptionPane.showMessageDialog(null, "Sepah");
+		
 	}
 
 }
